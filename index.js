a6e1beea3b1d1fb8845a2ed4ac10beaffd7c227c
pichu1298 <62480135+pichu1298@users.noreply.github.com>
2024-10-09 12:26:38 -0400

    afd

--- index.js
+++ index.js
@@ -25,14 +25,44 @@
 // //call function
 // changeColor();
 
-const DOMSelectors = {
-  header: document.querySelectorAll("h1"),
-  description: document.querySelectorAll(".card-desc"),
-  items: document.querySelectorAll("li"),
-};
+// const DOMSelectors = {
+//   header: document.querySelectorAll("h1"),
+//   description: document.querySelectorAll(".card-desc"),
+//   items: document.querySelectorAll("li"),
+// };
 let button = document.querySelector("form");
 button.addEventListener("submit", function (event) {
-  let input = document.querySelectorAll("#refresh");
   event.preventDefault();
-  console.log(input.value);
 });
+preventDefault();
+
+// const item = document.querySelectorAll("li");
+// const items = Array.from(item); // make nodelist into array
+// console.log(item);
+// //Node List not an array, but has several of the array methods Does not have filter.
+
+// items.forEach((el) => (el.style.color = "red")); //selects all li and changes color to red.
+// //iterate/loop over array and for each item and for each element make the color red.
+// const buttons = document.querySelectorAll("button");
+// buttons.forEach((btn) =>
+//   btn.addEventListener("click", function (event) {
+//     console.log(event.target.textContent); // grabs the specific button
+//   })
+// );
+
+// DOMSelectors.container.insertAdjacentHTML(
+//   beforeend,
+//   `<div class = "card">
+// <h2 class = "cardheader">${album.name}</h2>
+
+// </div>`
+// );
+
+// let album = {
+//   name: "yay",
+// };
+
+// //event listener for form,
+// get values from form for widget object (widget is generac term)
+//create the card insert the card
+//find remove buttons and add event listeners
